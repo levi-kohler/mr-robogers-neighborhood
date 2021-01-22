2ea060716fcb7e8e87d1731c84a498b3d0adf3d6
Levi Kohler <kohler.la01@gmail.com>
2021-01-22 09:59:26 -0800

    write first test and attempt to pass

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Create a web application that takes a number from a user and returns a range of numbers from 0 to the user inputted number with the following exceptions:
+
+// Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+
+// Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+
+// Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
+
+// // Describe: userInput()
+// Test: "It should return 0 if 0 is inputted."
+// Expect: (userInput(0).toEqual(0);
+
+function userInput(input) {
+  return 0;
+}
+userInput(0);
+
+console.log(0)
+
+// UI Logic
